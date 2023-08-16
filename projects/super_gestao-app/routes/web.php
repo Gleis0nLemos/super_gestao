c6080aabc48ccd447186bcdcd679af93dcdeb72d
Gleison Lemos <gleison04lemos@gmail.com>
2023-08-16 20:55:06 -0300

    feat: seeding parameters

--- projects/super_gestao-app/routes/web.php
+++ projects/super_gestao-app/routes/web.php
@@ -22,7 +22,20 @@ Route::get('/', [PrincipalController::class, 'principal']);
 
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
 
-Route::get('/contato    ', [ContatoController::class, 'contato']);
+Route::get('/contato', [ContatoController::class, 'contato']);
+
+Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}', function(string $nome, string $categoria, string $assunto, string $mensagem) {
+    echo 'Estamos aqui '.$nome;
+    echo '<br>';
+    echo $categoria;
+    echo '<br>';
+    echo $assunto;
+    echo '<br>';
+    echo $mensagem;
+    echo '<br>';
+
+});
+
 
 
 
