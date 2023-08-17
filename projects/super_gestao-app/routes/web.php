9d46e861ad1c8b77a745c0199e4513f93a5644a7
Gleison Lemos <gleison04lemos@gmail.com>
2023-08-17 15:42:34 -0300

    feat: redirect routes

--- projects/super_gestao-app/routes/web.php
+++ projects/super_gestao-app/routes/web.php
@@ -30,8 +30,15 @@ Route::prefix('/app')->group(function () {
     Route::get('/clientes', function () {return 'clientes';})->name('app.clientes');
     Route::get('/fornecedores', function () {return 'fornecedores';})->name('app.fornecedores');
     Route::get('/produtos', function () {return 'produtos';})->name('app.produtos');
-});
+}); 
 
+Route::get('/rota1', function () {
+    echo '1';   
+})->name('site.rota1');
 
+Route::get('/rota2', function () {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
 
+#Route::redirect('rota2', 'rota1');
 
