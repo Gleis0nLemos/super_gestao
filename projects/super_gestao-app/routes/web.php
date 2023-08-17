22c8a109d186b717028e39f76fb4b2697b08b0d2
Gleison Lemos <gleison04lemos@gmail.com>
2023-08-17 17:00:49 -0300

    feat: forwarding route parameters to controllers

--- projects/super_gestao-app/routes/web.php
+++ projects/super_gestao-app/routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\TesteController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -32,15 +34,7 @@ Route::prefix('/app')->group(function () {
     Route::get('/produtos', function () {return 'produtos';})->name('app.produtos');
 }); 
 
-Route::get('/rota1', function () {
-    echo '1';   
-})->name('site.rota1');
-
-Route::get('/rota2', function () {
-    return redirect()->route('site.rota1');
-})->name('site.rota2');
-
-#Route::redirect('rota2', 'rota1');
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
 
 Route::fallback(function () {
     echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial';
