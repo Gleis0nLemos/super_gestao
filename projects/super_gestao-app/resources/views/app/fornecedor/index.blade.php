be119d569506666249e3aae9bd2db08d8745ec57
Gleison Lemos <gleison04lemos@gmail.com>
2023-08-17 19:34:20 -0300

    feat: including comments and pure php blocks

---
+++ projects/super_gestao-app/resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fornecedor</title>
+</head>
+<body>
+    <h3>Fornecedor</h3>
+    {{--Fica o comentário que será ignorado pelo blade--}}
+    @php
+        echo 'Olá mundo!';
+    @endphp
+</body>
+</html>
